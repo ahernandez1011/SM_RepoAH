ccc19646b7406589568d0342f717be04e02e8990
Ana Hernández <analidia1025@gmail.com>
2026-07-16 10:33:52 -0600

    90% de recuperar el acceso

---
+++ SM_API_AH/Models/RecuperarAccesoRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SM_API_AH.Models
+{
+    public class RecuperarAccesoRequestModel
+    {
+        [Required]
+        public string CorreoElectronico { get; set; } = string.Empty;
+    }
+}
