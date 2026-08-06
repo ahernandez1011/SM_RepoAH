2c9c1369f1587e1852a1ed5c7f819b4e5c992336
Ana Hernández <analidia1025@gmail.com>
2026-08-06 12:40:42 -0600

    Cancelar Solicitud

--- SM_WEB_AH/wwwroot/js/consultarSolicitudes.js
+++ SM_WEB_AH/wwwroot/js/consultarSolicitudes.js
@@ -7,5 +7,41 @@
             url: 'https://cdn.datatables.net/plug-ins/2.3.4/i18n/es-ES.json'
         }
     });
+});
 
+$(document).on("click", ".btn-cancelar", function () {
+
+    var consecutivo = $(this).data("consecutivo");
+    var titulo = $(this).data("titulo");
+
+    Swal.fire({
+        text: "¿Desea cancelar la solicitud " + titulo + "?",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Sí",
+        cancelButtonText: "No"
+    }).then((result) => {
+        if (!result.isConfirmed)
+            return;
+
+        $.ajax({
+            url: "/Solicitud/CancelarSolicitud",
+            method: "POST",
+            data: {
+                consecutivoSolicitud: consecutivo
+            },
+            dataType: "json",
+            success: function (data) {
+
+                swal.fire({
+                    title: "Información",
+                    text: data,
+                    icon: "info",
+                    confirmButtonText: "Aceptar"
+                }).then(() => {
+                    location.reload();
+                });
+            }
+        })
+    });
 });
