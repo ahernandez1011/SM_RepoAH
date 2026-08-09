1730f90f55869e0c5163e8f80a51b55fcc9f06e1
Ana Hernández <analidia1025@gmail.com>
2026-08-08 19:31:25 -0600

    Acceso a la pantalla de contacto

---
+++ SM_API_AH/Controllers/ContactoController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SM_API_AH.Models;
+using SM_API_AH.Services;
+
+namespace SM_API_AH.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactoController(IConfiguration _config, IUtilesService _utiles) : ControllerBase
+    {
+
+        [HttpGet("ConsultarSolicitudesChatAPI")]
+        public IActionResult ConsultarSolicitudesChatAPI()
+        {
+            using var context = new SqlConnection(_config["ConnectionStrings:DefaultConnection"]);
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@ConsecutivoUsuario", _utiles.ObtenerConsecutivoToken());
+            parameters.Add("@ConsecutivoRol", _utiles.ObtenerConsecutivoRolToken());
+
+            var response = context.Query<SolicitudChatResponseModel>("spConsultarSolicitudesAbiertas", parameters);
+
+            if (response.Any())
+            {
+                return Ok(response);
+            }
+
+            return NotFound("No se han encontrado solicitudes abiertas en este momento");
+        }
+    }
+}
---
+++ SM_API_AH/Models/SolicitudChatResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SM_API_AH.Models
+{
+    public class SolicitudChatResponseModel
+    {
+        public int Consecutivo { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string NombreInterlocutor { get; set; } = string.Empty;
+    }
+}
