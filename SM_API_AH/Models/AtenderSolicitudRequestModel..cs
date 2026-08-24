07c72f4c04657334fc928b8f2658f864f8cf3769
Ana Hernández <analidia1025@gmail.com>
2026-08-24 12:07:51 -0600

    ATENCIÓN DE SOLICITUDES

---
+++ SM_API_AH/Models/AtenderSolicitudRequestModel..cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SM_API_AH.Models
+{
+    public class AtenderSolicitudRequestModel
+    {
+        [Required]
+        public int ConsecutivoSolicitud { get; set; }
+        [Required]
+        public string Solucion { get; set; } = string.Empty;
+    }
+}
--- SM_WEB_AH/Controllers/SolicitudController.cs
+++ SM_WEB_AH/Controllers/SolicitudController.cs
@@ -2,6 +2,7 @@
 using SM_WEB_AH.Filters;
 using SM_WEB_AH.Models;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace SM_WEB_AH.Controllers
 {
@@ -89,6 +90,18 @@ namespace SM_WEB_AH.Controllers
             client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("Token"));
             var urlApi = _config["Valores:UrlApi"] + "Solicitud/CancelarSolicitudAPI?consecutivoSolicitud=" + consecutivoSolicitud;
             var response = client.DeleteAsync(urlApi).Result;
+
+            return Json(response.Content.ReadAsStringAsync().Result);
+        }
+
+        [HttpPost]
+        public IActionResult AtenderSolicitud(int consecutivo, string solucion)
+        {
+            using var client = _http.CreateClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("Token"));
+            var url = _config["Valores:UrlApi"] + "Solicitud/AtenderSolicitudAPI";
+            var response = client.PutAsJsonAsync(url, new { ConsecutivoSolicitud = consecutivo, Solucion = solucion }).Result;
 
             return Json(response.Content.ReadAsStringAsync().Result);
         }
